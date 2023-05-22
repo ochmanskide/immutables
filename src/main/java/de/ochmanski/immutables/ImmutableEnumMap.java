c26d65b42272042acaed43af1e4c1f74744a444b
Lukasz Ochmanski <lukasz.ochmanski@stadlerrail.com>
2023-05-22 07:13:10 +0200

    new EnumMap<>

--- src/main/java/de/ochmanski/immutables/ImmutableEnumMap.java
+++ src/main/java/de/ochmanski/immutables/ImmutableEnumMap.java
@@ -6,6 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.UnmodifiableView;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.function.IntFunction;
 
@@ -57,6 +58,89 @@ public class ImmutableEnumMap<K extends Enum<@NotNull K> & Equalable<@NotNull K>
 
   private static class EmptyValue implements Equalable<ImmutableEnumMap.@NotNull EmptyValue>
   {
+  }
+
+  /**
+   * This method is not supported.
+   * <p>You must provide a generic type for an empty collection.
+   * <p>use method: {@link #ofGenerator(IntFunction, IntFunction)} instead.
+   * <p>Example usage:
+   * <pre>
+   *   {@code
+   *   final IMap<Dummy> actual = IMap.ofGenerator(Dummy[]::new);
+   *   final IMap<String> actual = IMap.ofGenerator(String[]::new);
+   *   final IMap<Integer> actual = IMap.ofGenerator(Integer[]::new);
+   *   }
+   * </pre>
+   */
+  @Contract(value = "-> fail", pure = true)
+  static void of()
+  {
+    throw new UnsupportedOperationException("Please pass array generator type to the method. "
+        + "For example: IMap.ofGenerator(String[]::new)");
+  }
+
+  /**
+   * Example usage:
+   * <pre>
+   *   {@code
+   *   final IMap<Dummy> actual = IMap.ofGenerator(Dummy[]::new);
+   *   final IMap<String> actual = IMap.ofGenerator(String[]::new);
+   *   final IMap<Integer> actual = IMap.ofGenerator(Integer[]::new);
+   *   }
+   * </pre>
+   */
+  @NotNull
+  @UnmodifiableView
+  @Contract(value = "_, _ -> new", pure = true)
+  static <K extends Enum<@NotNull K> & Equalable<@NotNull K>, V extends Equalable<@NotNull V>> IMap<@NotNull K, @NotNull V>
+  ofGenerator(@NotNull final IntFunction<@NotNull K[]> key, @NotNull final IntFunction<@NotNull V[]> value)
+  {
+    return ImmutableEnumMap.<K, V>builder().key(key).value(value).build();
+  }
+
+  @NotNull
+  @UnmodifiableView
+  @Contract(value = " _, _ -> new", pure = true)
+  static <K extends Enum<@NotNull K> & Equalable<@NotNull K>, V extends Equalable<@NotNull V>> IMap<@NotNull K, @NotNull V> of(
+      @NotNull final K k1, @NotNull final V v1)
+  {
+    final Map<@NonNull @NotNull K, V> map = new EnumMap<K, V>(k1, v1);
+    map.put(k1, v1);
+    return ImmutableEnumMap.<K, V>builder().map(map).build();
+  }
+
+  @NotNull
+  @UnmodifiableView
+  @Contract(value = " _, _, _, _ -> new", pure = true)
+  static <K extends Enum<@NotNull K> & Equalable<@NotNull K>, V extends Equalable<@NotNull V>> IMap<@NotNull K, @NotNull V> of(
+      @NotNull final K k1, @NotNull final V v1,
+      @NotNull final K k2, @NotNull final V v2)
+  {
+    return ImmutableEnumMap.<K, V>builder().map(EnumMap.of(k1, v1, k2, v2)).build();
+  }
+
+  @NotNull
+  @UnmodifiableView
+  @Contract(value = " _, _, _, _, _, _ -> new", pure = true)
+  static <K extends Enum<@NotNull K> & Equalable<@NotNull K>, V extends Equalable<@NotNull V>> IMap<@NotNull K, @NotNull V> of(
+      @NotNull final K k1, @NotNull final V v1,
+      @NotNull final K k2, @NotNull final V v2,
+      @NotNull final K k3, @NotNull final V v3)
+  {
+    return ImmutableEnumMap.<K, V>builder().map(EnumMap.of(k1, v1, k2, v2, k3, v3)).build();
+  }
+
+  @NotNull
+  @UnmodifiableView
+  @Contract(value = " _, _, _, _, _, _, _, _ -> new", pure = true)
+  static <K extends Enum<@NotNull K> & Equalable<@NotNull K>, V extends Equalable<@NotNull V>> IMap<@NotNull K, @NotNull V> of(
+      @NotNull final K k1, @NotNull final V v1,
+      @NotNull final K k2, @NotNull final V v2,
+      @NotNull final K k3, @NotNull final V v3,
+      @NotNull final K k4, @NotNull final V v4)
+  {
+    return ImmutableEnumMap.<K, V>builder().map(EnumMap.of(k1, v1, k2, v2, k3, v3, k4, v4)).build();
   }
 
   /**

13a36ae7e2beb15a7e3c7a50bc0f5e50d649d066
Lukasz Ochmanski <lukasz.ochmanski@stadlerrail.com>
2023-07-07 10:40:24 +0200

    noneMatch = isNotIn

--- src/main/java/de/ochmanski/immutables/Fluent.java
+++ src/main/java/de/ochmanski/immutables/Fluent.java
@@ -22,14 +22,39 @@ public interface Fluent<E extends Enum<@NotNull E>> extends Equalable<@NotNull E
     return this == other;
   }
 
-  default boolean isNotIn(@NotNull final E... array)
+  default boolean anyMatch(@NotNull final E @NotNull ... array)
   {
-    return !isIn(array);
+    return isIn(array);
   }
 
-  default boolean isIn(@NotNull final E... array)
+  default boolean anyMatch(@NotNull final List<@NotNull E> elements)
   {
-    return isIn(Arrays.asList(array));
+    return isIn(elements);
+  }
+
+  default boolean allMatch(@NotNull final E @NotNull ... array)
+  {
+    return allMatch(Arrays.asList(array));
+  }
+
+  default boolean allMatch(@NotNull final List<@NotNull E> elements)
+  {
+    return elements.stream().allMatch(this::isEqualTo);
+  }
+
+  default boolean noneMatch(@NotNull final E @NotNull ... array)
+  {
+    return isNotIn(array);
+  }
+
+  default boolean noneMatch(@NotNull final List<@NotNull E> elements)
+  {
+    return isNotIn(elements);
+  }
+
+  default boolean isNotIn(@NotNull final E @NotNull ... array)
+  {
+    return !isIn(array);
   }
 
   default boolean isNotIn(@NotNull final List<@NotNull E> elements)
@@ -37,17 +62,24 @@ public interface Fluent<E extends Enum<@NotNull E>> extends Equalable<@NotNull E
     return !isIn(elements);
   }
 
+  default boolean isIn(@NotNull final E @NotNull ... array)
+  {
+    return isIn(Arrays.asList(array));
+  }
+
   default boolean isIn(@NotNull final List<@NotNull E> elements)
   {
     return elements.contains(this);
   }
 
+  //@MustBeInvokedByOverriders
   @NotNull
   static <E> Stream<@NotNull E> createStream(@NotNull final Class<@NotNull E> clazz)
   {
     return Arrays.stream(getEnumConstants(clazz));
   }
 
+  //@MustBeInvokedByOverriders
   static <E> void forEachHelper(@NotNull final Class<@NotNull E> clazz, @NotNull final Consumer<@NotNull E> consumer)
   {
     Arrays.asList(getEnumConstants(clazz)).forEach(consumer);

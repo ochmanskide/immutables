b8b7d88607192c5390e397edab0406ba28e6e73e
Lukasz Ochmanski <lukasz.ochmanski@stadlerrail.com>
2023-10-19 16:31:10 +0200

    Fix Equalable interface

--- src/main/java/de/ochmanski/immutables/equalable/Equalable.java
+++ src/main/java/de/ochmanski/immutables/equalable/Equalable.java
@@ -7,21 +7,13 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public interface Equalable<T>
+public interface Equalable<T extends Equalable<@NotNull T>>
 {
-
-  @Contract(value = "null -> true", pure = true)
-  default boolean isNotEqualTo(@Nullable final T other)
-  {
-    return !isEqualTo(other);
-  }
-
-  @Contract(value = "null -> false", pure = true)
-  default boolean isEqualTo(@Nullable final T other)
-  {
-    return areEqual(this, other);
-  }
 
   @Contract(pure = true)
   default boolean anyMatch(@NotNull final T @NotNull ... array)
@@ -36,6 +28,58 @@ public interface Equalable<T>
   }
 
   @Contract(pure = true)
+  default boolean anyMatchIgnoreCase(@NotNull final List<@NotNull String> b,
+    @NotNull final Function<? super @NotNull Equalable<@NotNull T>, @NotNull String> a)
+  {
+    final String s = a.apply(this);
+    return Equalable.<String>anyMatchIgnoreCase(b, s);
+  }
+
+  @Contract(pure = true)
+  static <S> boolean anyMatchIgnoreCase(@NotNull final List<@NotNull S> s, @NotNull final Function<? super @NotNull S,
+    @NotNull String> a, @NotNull final List<@NotNull String> b)
+  {
+    return Equalable.<S>anyMatchIgnoreCase(s.stream().map(a).toList(), b);
+  }
+
+  @Contract(pure = true)
+  default boolean anyMatchIgnoreCase(@NotNull final Function<? super @NotNull Equalable<@NotNull T>,
+    @NotNull List<@NotNull String>> a, @NotNull final List<@NotNull String> b)
+  {
+    return Equalable.<Equalable<T>>anyMatchIgnoreCase(this, a, b);
+  }
+
+  @Contract(pure = true)
+  static <S> boolean anyMatchIgnoreCase(@NotNull final S s, @NotNull final Function<? super @NotNull S,
+    List<@NotNull String>> a, @NotNull final List<@NotNull String> b)
+  {
+    return Equalable.<S>anyMatchIgnoreCase(a.apply(s), b);
+  }
+
+  @Contract(pure = true)
+  static boolean anyMatchIgnoreCase(@NotNull final List<@NotNull String> a, @NotNull final List<@NotNull String> b)
+  {
+    final TreeSet<@NotNull String> sortedSet = a.stream()
+      .map(String::toUpperCase)
+      .sorted()
+      .collect(Collectors.toCollection(TreeSet::new));
+    return Equalable.<String>anyMatch(b, c -> sortedSet.contains(c.toUpperCase()));
+  }
+
+  @Contract(pure = true)
+  static boolean anyMatchIgnoreCase(@NotNull final List<@NotNull String> elements, @NotNull final String text)
+  {
+    return Equalable.<String>anyMatch(elements, p -> p.equalsIgnoreCase(text));
+  }
+
+  @Contract(pure = true)
+  static <T> boolean anyMatch(@NotNull final List<? extends @NotNull T> elements,
+    final Predicate<? super @NotNull T> predicate)
+  {
+    return elements.stream().anyMatch(predicate);
+  }
+
+  @Contract(pure = true)
   default boolean allMatch(@NotNull final T @NotNull ... array)
   {
     return allMatch(Arrays.asList(array));
@@ -44,7 +88,14 @@ public interface Equalable<T>
   @Contract(pure = true)
   default boolean allMatch(@NotNull final List<? extends @NotNull T> elements)
   {
-    return elements.stream().allMatch(this::isEqualTo);
+    return allMatch(elements, this::isEqualTo);
+  }
+
+  @Contract(pure = true)
+  static <T> boolean allMatch(@NotNull final List<? extends @NotNull T> elements,
+    final Predicate<? super @NotNull T> predicate)
+  {
+    return elements.stream().anyMatch(predicate);
   }
 
   @Contract(pure = true)
@@ -57,6 +108,13 @@ public interface Equalable<T>
   default boolean noneMatch(@NotNull final List<? extends @NotNull T> elements)
   {
     return isNotIn(elements);
+  }
+
+  @Contract(pure = true)
+  static <T> boolean noneMatch(@NotNull final List<? extends @NotNull T> elements,
+    final Predicate<? super @NotNull T> predicate)
+  {
+    return elements.stream().noneMatch(predicate);
   }
 
   @Contract(pure = true)
@@ -83,10 +141,46 @@ public interface Equalable<T>
     return elements.contains(this);
   }
 
+  @Contract(value = "null -> true", pure = true)
+  default boolean isNotEqualTo(@Nullable final T other)
+  {
+    return !isEqualTo(other);
+  }
+
+  @Contract(value = "null -> false", pure = true)
+  default boolean isEqualTo(@Nullable final T other)
+  {
+    return Equalable.<Equalable<T>>areEqual(this, other);
+  }
+
+  @Contract(value = "null -> true", pure = true)
+  default boolean isNotSameAs(@Nullable final T other)
+  {
+    return !isSameAs(other);
+  }
+
+  @Contract(value = "null -> false", pure = true)
+  default boolean isSameAs(@Nullable final T other)
+  {
+    return Equalable.<Equalable<T>>areTheSame(this, other);
+  }
+
+  @Contract(value = "null, !null -> true; !null, null -> true; null, null -> false", pure = true)
+  static <S extends Equalable<@NotNull S>> boolean areNotEqual(@Nullable final S a, @Nullable final S b)
+  {
+    return Equalable.<Equalable<S>>areNotEqual(a, b);
+  }
+
   @Contract(value = "null, !null -> true; !null, null -> true; null, null -> false", pure = true)
   static <S> boolean areNotEqual(@Nullable final S a, @Nullable final S b)
   {
-    return !areEqual(a, b);
+    return !Equalable.<S>areEqual(a, b);
+  }
+
+  @Contract(value = "null, !null -> false; !null, null -> false; null, null -> true", pure = true)
+  static <S extends Equalable<@NotNull S>> boolean areEqual(@Nullable final S a, @Nullable final S b)
+  {
+    return Equalable.<Equalable<S>>areEqual(a, b);
   }
 
   @Contract(value = "null, !null -> false; !null, null -> false; null, null -> true", pure = true)
@@ -94,4 +188,29 @@ public interface Equalable<T>
   {
     return Objects.equals(a, b);
   }
+
+  @Contract(value = "null, !null -> true; !null, null -> true; null, null -> false", pure = true)
+  static <S extends Equalable<@NotNull S>> boolean areNotTheSame(@Nullable final S a, @Nullable final S b)
+  {
+    return Equalable.<Equalable<S>>areNotTheSame(a, b);
+  }
+
+  @Contract(value = "null, !null -> true; !null, null -> true; null, null -> false", pure = true)
+  static <S> boolean areNotTheSame(@Nullable final S a, @Nullable final S b)
+  {
+    return !Equalable.<S>areTheSame(a, b);
+  }
+
+  @Contract(value = "null, !null -> false; !null, null -> false; null, null -> true", pure = true)
+  static <S extends Equalable<@NotNull S>> boolean areTheSame(@Nullable final S a, @Nullable final S b)
+  {
+    return Equalable.<Equalable<S>>areTheSame(a, b);
+  }
+
+  @Contract(value = "null, !null -> false; !null, null -> false; null, null -> true", pure = true)
+  static <S> boolean areTheSame(@Nullable final S a, @Nullable final S b)
+  {
+    return a == b;
+  }
+
 }
--- src/main/java/de/ochmanski/immutables/fluent/Fluent.java
+++ src/main/java/de/ochmanski/immutables/fluent/Fluent.java
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
@@ -17,7 +16,7 @@ public interface Fluent<F extends Enum<@NotNull F> & Fluent<? extends @NotNull F
   //@MustBeInvokedByOverriders
   @NotNull
   @Contract(value = "_ -> new", pure = true)
-  static <F extends Enum<@NotNull F> & Fluent<@NotNull F>> Stream<@NotNull F> createStream(
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> Stream<@NotNull F> createStream(
     @NotNull final Class<? extends @NotNull F> clazz)
   {
     final @NotNull F @NotNull [] enumConstants = getEnumConstants(clazz);
@@ -27,7 +26,7 @@ public interface Fluent<F extends Enum<@NotNull F> & Fluent<? extends @NotNull F
   //@MustBeInvokedByOverriders
   @NotNull
   @Contract(value = "_ -> new", pure = true)
-  static <F extends Enum<@NotNull F> & Fluent<@NotNull F>> Stream<@NotNull F> createStream(
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> Stream<@NotNull F> createStream(
     @NotNull final F @NotNull [] entries)
   {
     return Arrays.stream(entries);
@@ -35,7 +34,7 @@ public interface Fluent<F extends Enum<@NotNull F> & Fluent<? extends @NotNull F
 
   //@MustBeInvokedByOverriders
   @Contract(pure = true)
-  static <F extends Enum<@NotNull F> & Fluent<@NotNull F>> void forEachHelper(
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> void forEachHelper(
     @NotNull final Class<@NotNull F> clazz, @NotNull final Consumer<? super @NotNull F> consumer)
   {
     final @NotNull F @NotNull [] enumConstants = getEnumConstants(clazz);
@@ -43,7 +42,7 @@ public interface Fluent<F extends Enum<@NotNull F> & Fluent<? extends @NotNull F
   }
 
   @Contract(pure = true)
-  static <F extends Enum<@NotNull F> & Fluent<@NotNull F>> void forEach(@NotNull final F @NotNull [] entries,
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> void forEach(@NotNull final F @NotNull [] entries,
     @NotNull final Consumer<? super @NotNull F> consumer)
   {
     Arrays.asList(entries).forEach(consumer);
@@ -51,24 +50,26 @@ public interface Fluent<F extends Enum<@NotNull F> & Fluent<? extends @NotNull F
 
   @NotNull
   @Contract(pure = true)
-  static <F extends Enum<@NotNull F> & Fluent<@NotNull F>> F @NotNull [] getEnumConstants(
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> F @NotNull [] getEnumConstants(
     @NotNull final Class<? extends @NotNull F> enumClass)
   {
     return enumClass.getEnumConstants();
   }
 
   @Contract(value = "null, !null -> true; !null, null -> true; null, null -> false", pure = true)
-  static <F extends Enum<@NotNull F> & Fluent<@NotNull F>> boolean areNotEqual(@Nullable final F a,
-    @Nullable final F b)
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> boolean areNotEqual(
+    @Nullable final Fluent<@NotNull F> a,
+    @Nullable final Fluent<@NotNull F> b)
   {
     return !areEqual(a, b);
   }
 
   @Contract(value = "null, !null -> false; !null, null -> false; null, null -> true", pure = true)
-  static <F extends Enum<@NotNull F> & Fluent<@NotNull F>> boolean areEqual(@Nullable final F a,
-    @Nullable final F b)
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> boolean areEqual(
+    @Nullable final Fluent<@NotNull F> a,
+    @Nullable final Fluent<@NotNull F> b)
   {
-    return Objects.equals(a, b);
+    return Fluent.<F>areTheSame(a, b);
   }
 
   /**
@@ -76,9 +77,32 @@ public interface Fluent<F extends Enum<@NotNull F> & Fluent<? extends @NotNull F
    */
   @Override
   @Contract(value = "null -> false", pure = true)
-  default boolean isEqualTo(@Nullable final Fluent<? extends @NotNull F> other)
+  default boolean isEqualTo(@Nullable final Fluent<@NotNull F> other)
   {
-    return this == other;
+    return isSameAs(other);
+  }
+
+  @Override
+  @Contract(value = "null -> false", pure = true)
+  default boolean isSameAs(@Nullable final Fluent<@NotNull F> other)
+  {
+    return Fluent.<F>areTheSame(this, other);
+  }
+
+  @Contract(value = "null, !null -> true; !null, null -> true; null, null -> false", pure = true)
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> boolean areNotTheSame(
+    @Nullable final Fluent<@NotNull F> a,
+    @Nullable final Fluent<@NotNull F> b)
+  {
+    return !Fluent.<F>areTheSame(a, b);
+  }
+
+  @Contract(value = "null, !null -> false; !null, null -> false; null, null -> true", pure = true)
+  static <F extends Enum<@NotNull F> & Fluent<? extends @NotNull F>> boolean areTheSame(
+    @Nullable final Fluent<@NotNull F> a,
+    @Nullable final Fluent<@NotNull F> b)
+  {
+    return Equalable.<F>areTheSame(a, b);
   }
 
 }

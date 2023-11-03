fd31a8d49cd2610ae5fc3a2012e12ecb15cbe089
Lukasz Ochmanski <github@ochmanski.de>
2023-11-03 22:20:29 +0100

    clean ImmutableSet

--- src/main/java/de/ochmanski/immutables/immutable/ImmutableSet.java
+++ src/main/java/de/ochmanski/immutables/immutable/ImmutableSet.java
@@ -40,13 +40,28 @@ public class ImmutableSet<E> implements ISet<@NotNull E>
   @Builder.Default
   IntFunction<@NonNull @NotNull E @NonNull @NotNull []> key = defaultKey();
 
+  //<editor-fold defaultstate="collapsed" desc="1. eager static initializers">
   @NotNull
-  @SuppressWarnings({ UNCHECKED, RAWTYPES })
+  @Unmodifiable
+  @UnmodifiableView
+  @Contract(pure = true)
+  public static <E> ImmutableSet<@NotNull E> empty()
+  {
+    return EMPTY_SET;
+  }
+
+  private static final ImmutableSet EMPTY_SET = ImmutableSet.builder().build();
+
+  @NotNull
+  @SuppressWarnings({UNCHECKED, RAWTYPES})
   @Contract(value = " -> new", pure = true)
   private static <S> IntFunction<@NotNull S @NotNull []> defaultKey()
   {
-    return (IntFunction)Object @NotNull []::new;
+    return (IntFunction) Object @NotNull []::new;
   }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="2. static factory methods">
 
   @NotNull
   @UnmodifiableView
@@ -120,141 +135,8 @@ public class ImmutableSet<E> implements ISet<@NotNull E>
   @NotNull
   @Contract(value = "_ -> new", pure = true)
   private static <K, V> IMap.@Unmodifiable @UnmodifiableView @NotNull Entry<@NotNull K, @NotNull V> toImmutableEntry(
-    @NotNull final Map.@NotNull Entry<@NotNull K, @NotNull V> entry)
-  {
+    @NotNull final Map.@NotNull Entry<@NotNull K, @NotNull V> entry) {
     return IMap.Entry.<@NotNull K, @NotNull V>builder().key(entry.getKey()).value(entry.getValue()).build();
-  }
-
-  @NotNull
-  @Unmodifiable
-  @UnmodifiableView
-  @Contract(pure = true)
-  public static <E> ImmutableSet<@NotNull E> empty()
-  {
-    return EMPTY_SET;
-  }
-
-  private static final ImmutableSet EMPTY_SET = ImmutableSet.builder().build();
-
-  /**
-   * Returns the number of elements in this set.
-   *
-   * @return the number of elements in this set
-   */
-  public int size()
-  {
-    return set.size();
-  }
-
-  /**
-   * Returns {@code true} if this set contains no elements.
-   *
-   * @return {@code true} if this set contains no elements
-   */
-  public boolean isEmpty()
-  {
-    return set.isEmpty();
-  }
-
-  /**
-   * Returns {@code true} if this set contains the specified element. More formally, returns {@code true} if and only if
-   * this set contains at least one element {@code e} such that {@code Objects.equals(o, e)}.
-   *
-   * @param o element whose presence in this set is to be tested
-   * @return {@code true} if this set contains the specified element
-   */
-  public boolean contains(@NotNull final E o)
-  {
-    return set.contains(o);
-  }
-
-  /**
-   * Returns a deep copy of this {@code ArraySet} instance.  (The elements themselves are also copied.)
-   *
-   * @return a clone of this {@code ArraySet} instance
-   */
-  @NotNull
-  @UnmodifiableView
-  @Contract(value = " -> new", pure = true)
-  public ImmutableSet<@NotNull E> deepClone()
-  {
-    return toBuilder().build();
-  }
-
-  /**
-   * Returns an array containing all the elements in this set in proper sequence (from first to last element).
-   *
-   * <p>The returned array will be "safe" in that no references to it are
-   * maintained by this set.  (In other words, this method must allocate a new array).  The caller is thus free to
-   * modify the returned array.
-   *
-   * <p>This method acts as bridge between array-based and collection-based
-   * APIs.
-   *
-   * @return an array containing all the elements in this set in proper sequence
-   */
-  @NotNull
-  @Override
-  @Contract(value = "-> new", pure = true)
-  public E @NotNull [] toArray()
-  {
-    return set.toArray(getKey());
-  }
-
-  @Override
-  @Contract(pure = true)
-  public void forEach(@NotNull final Consumer<? super @NotNull E> consumer)
-  {
-    unwrap().forEach(consumer);
-  }
-
-  @Override
-  @Contract(pure = true)
-  public void forEachRemaining(@NotNull final Consumer<? super @NotNull E> consumer)
-  {
-    iterator().forEachRemaining(consumer);
-  }
-
-  /**
-   * Returns an iterator over the elements in this set.  The elements are returned in no particular order (unless this
-   * set is an instance of some class that provides a guarantee).
-   *
-   * @return an iterator over the elements in this set
-   */
-  @NotNull
-  @Override
-  @Contract(pure = true)
-  public Iterator<@NotNull E> iterator()
-  {
-    return unwrap().iterator();
-  }
-
-  /**
-   * Returns a sequential {@code Stream} with this collection as its source.
-   *
-   * @return a sequential {@code Stream} over the elements in this collection
-   * @implSpec The default implementation creates a sequential {@code Stream} from the collection's
-   *   {@code Spliterator}.
-   * @since 1.8
-   */
-  @NotNull
-  @Override
-  @UnmodifiableView
-  @Contract(value = " -> new", pure = true)
-  public Stream<@NotNull E> stream()
-  {
-    return unwrap().stream();
-  }
-
-  @NotNull
-  @Override
-  @UnmodifiableView
-  @Contract(value = " -> new", pure = true)
-  public Set<@NotNull E> unwrap()
-  {
-    return set.isEmpty()
-      ? Collections.checkedSet(Set.of(), getComponentType())
-      : Collections.checkedSet(Set.copyOf(set), getComponentType());
   }
 
   @NotNull
@@ -282,7 +164,7 @@ public class ImmutableSet<E> implements ISet<@NotNull E>
   @Unmodifiable
   @UnmodifiableView
   @Contract(value = " _ -> new", pure = true)
-  static <S> Class<@NotNull S> getComponentTypeFromConstructor(
+  private static <S> Class<@NotNull S> getComponentTypeFromConstructor(
     @NotNull final IntFunction<@NotNull S @NotNull []> constructor) {
     return ICollection.<@NotNull S>getComponentTypeFromConstructor(constructor);
   }
@@ -293,7 +175,117 @@ public class ImmutableSet<E> implements ISet<@NotNull E>
   public static <S> ImmutableSet<@NotNull S> of(ImmutableList<@NotNull S> immutableList) {
     return ImmutableSet.<@NotNull S>of(immutableList.unwrap(), immutableList.getKey());
   }
+  //</editor-fold>
 
+  //<editor-fold defaultstate="collapsed" desc="3. implementation of IList interface">
+
+  /**
+   * Returns the number of elements in this set.
+   *
+   * @return the number of elements in this set
+   */
+  public int size() {
+    return set.size();
+  }
+
+  /**
+   * Returns {@code true} if this set contains no elements.
+   *
+   * @return {@code true} if this set contains no elements
+   */
+  public boolean isEmpty() {
+    return set.isEmpty();
+  }
+
+  /**
+   * Returns {@code true} if this set contains the specified element. More formally, returns {@code true} if and only if
+   * this set contains at least one element {@code e} such that {@code Objects.equals(o, e)}.
+   *
+   * @param o element whose presence in this set is to be tested
+   * @return {@code true} if this set contains the specified element
+   */
+  public boolean contains(@NotNull final E o) {
+    return set.contains(o);
+  }
+
+  /**
+   * Returns a deep copy of this {@code ArraySet} instance.  (The elements themselves are also copied.)
+   *
+   * @return a clone of this {@code ArraySet} instance
+   */
+  @NotNull
+  @UnmodifiableView
+  @Contract(value = " -> new", pure = true)
+  public ImmutableSet<@NotNull E> deepClone() {
+    return toBuilder().build();
+  }
+
+  /**
+   * Returns an array containing all the elements in this set in proper sequence (from first to last element).
+   *
+   * <p>The returned array will be "safe" in that no references to it are
+   * maintained by this set.  (In other words, this method must allocate a new array).  The caller is thus free to
+   * modify the returned array.
+   *
+   * <p>This method acts as bridge between array-based and collection-based
+   * APIs.
+   *
+   * @return an array containing all the elements in this set in proper sequence
+   */
+  @NotNull
+  @Override
+  @Contract(value = "-> new", pure = true)
+  public E @NotNull [] toArray() {
+    return set.toArray(getKey());
+  }
+
+  @Override
+  @Contract(pure = true)
+  public void forEach(@NotNull final Consumer<? super @NotNull E> consumer) {
+    set.forEach(consumer);
+  }
+
+  @Override
+  @Contract(pure = true)
+  public void forEachRemaining(@NotNull final Consumer<? super @NotNull E> consumer) {
+    iterator().forEachRemaining(consumer);
+  }
+
+  /**
+   * Returns an iterator over the elements in this set.  The elements are returned in no particular order (unless this
+   * set is an instance of some class that provides a guarantee).
+   *
+   * @return an iterator over the elements in this set
+   */
+  @NotNull
+  @Override
+  @Contract(pure = true)
+  public Iterator<@NotNull E> iterator() {
+    return set.iterator();
+  }
+
+  /**
+   * Returns a sequential {@code Stream} with this collection as its source.
+   *
+   * @return a sequential {@code Stream} over the elements in this collection
+   * @implSpec The default implementation creates a sequential {@code Stream} from the collection's
+   * {@code Spliterator}.
+   * @since 1.8
+   */
+  @NotNull
+  @Override
+  @UnmodifiableView
+  @Contract(value = " -> new", pure = true)
+  public Stream<@NotNull E> stream() {
+    return set.stream();
+  }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="4. Positional Access Operations">
+
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="5. converters to family classes">
   @NotNull
   @Override
   @Unmodifiable
@@ -302,4 +294,17 @@ public class ImmutableSet<E> implements ISet<@NotNull E>
   public ImmutableList<@NotNull E> toList() {
     return ImmutableList.<@NotNull E>of(unwrap(), key);
   }
+  //</editor-fold>
+
+  //<editor-fold defaultstate="collapsed" desc="6. bridge for Java Collection API">
+  @NotNull
+  @Override
+  @UnmodifiableView
+  @Contract(value = " -> new", pure = true)
+  public Set<@NotNull E> unwrap() {
+    return set.isEmpty()
+      ? Collections.checkedSet(Set.of(), getComponentType())
+      : Collections.checkedSet(Set.copyOf(set), getComponentType());
+  }
+  //</editor-fold>
 }
